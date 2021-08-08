98415a925fabe3647516fe1ba559f55357dc6783
qwertytoki <qwertytoki@gmail.com>
2021-08-08 23:08:02 +0800

    tiny update

--- pages/index.js
+++ pages/index.js
@@ -21,6 +21,8 @@ export default function Home({ allPostsData }) {
               <Link href={`/posts/${id}`}>
                 <a>{title}</a>
               </Link>
+              <br />
+              <span>{date}</span>
             </li>
           ))}
         </ul>
