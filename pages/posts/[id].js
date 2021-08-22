e599dc08f039ac3d9a697256c7fbd8de37d264ef
qwertytoki <qwertytoki@gmail.com>
2021-08-23 01:40:11 +0800

    format

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,33 +1,31 @@
 import Layout from "../../components/layout";
-import { getAllPostIds,getPostData } from "../../lib/posts";
+import { getAllPostIds, getPostData } from "../../lib/posts";
 
-export async function getStaticProps({ params}){
-    const postData = await getPostData(params.id);
-    return{
-        props: {
-            postData,
-        },
-    };
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
+  };
 }
 
-export async function getStaticPaths(){
-    const paths = getAllPostIds();
-    return {
-        paths,
-        fallback: false,
-    };
+export async function getStaticPaths() {
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: false,
+  };
 }
 
-export default function Post({ postData}){
-    return (
-        <Layout>
-            <div>
-                <h1>{postData.title}</h1>
-                <div>
-                    {postData.date}
-                </div>
-                <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-            </div>
-        </Layout>
-    );
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      <div>
+        <h1>{postData.title}</h1>
+        <div>{postData.date}</div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </div>
+    </Layout>
+  );
 }
